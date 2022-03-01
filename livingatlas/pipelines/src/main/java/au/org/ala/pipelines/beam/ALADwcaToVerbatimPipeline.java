52161f4819c8b2b6b6708086c338e1778afdbf2e
nvolik <nikolay.volik@hotmail.com>
2022-03-01 15:16:21 +0100

    Add generic PipelinesException

--- livingatlas/pipelines/src/main/java/au/org/ala/pipelines/beam/ALADwcaToVerbatimPipeline.java
+++ livingatlas/pipelines/src/main/java/au/org/ala/pipelines/beam/ALADwcaToVerbatimPipeline.java
@@ -2,6 +2,7 @@ package au.org.ala.pipelines.beam;
 
 import au.org.ala.pipelines.options.DwcaToVerbatimPipelineOptions;
 import au.org.ala.pipelines.util.VersionInfo;
+import au.org.ala.utils.ALAFsUtils;
 import au.org.ala.utils.CombinedYamlConfiguration;
 import au.org.ala.utils.ValidationUtils;
 import java.io.File;
@@ -10,10 +11,12 @@ import java.nio.file.Paths;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
-import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.codehaus.plexus.util.FileUtils;
 import org.gbif.api.model.pipelines.StepType;
+import org.gbif.pipelines.common.PipelinesException;
+import org.gbif.pipelines.common.PipelinesVariables;
 import org.gbif.pipelines.common.beam.DwcaIO;
 import org.gbif.pipelines.common.beam.metrics.MetricsHandler;
 import org.gbif.pipelines.common.beam.options.PipelinesOptionsFactory;
@@ -33,7 +36,32 @@ public class ALADwcaToVerbatimPipeline {
         PipelinesOptionsFactory.create(DwcaToVerbatimPipelineOptions.class, combinedArgs);
     options.setMetaFileName(ValidationUtils.VERBATIM_METRICS);
     PipelinesOptionsFactory.registerHdfs(options);
-    run(options);
+    runWithLocking(options);
+  }
+
+  /**
+   * Run a load for the supplied dataset, creating a lock to prevent other load process loading the
+   * same archive.
+   */
+  private static void runWithLocking(DwcaToVerbatimPipelineOptions options) throws IOException {
+    // check for a lock file - if there isn't one, create one.
+    boolean okToProceed = ALAFsUtils.checkAndCreateLockFile(options);
+
+    if (okToProceed) {
+      MDC.put("datasetId", options.getDatasetId());
+      MDC.put("attempt", options.getAttempt().toString());
+      MDC.put("step", StepType.DWCA_TO_VERBATIM.name());
+      run(options);
+
+      if (options.isDeleteLockFileOnExit()) {
+        ALAFsUtils.deleteLockFile(options);
+      }
+
+    } else {
+      log.info(
+          "Dataset {} is locked. Will not attempt to loaded. Remove lockdir to proceed,",
+          options.getDatasetId());
+    }
   }
 
   public static void run(DwcaToVerbatimPipelineOptions options) throws IOException {
@@ -58,7 +86,7 @@ public class ALADwcaToVerbatimPipeline {
       Path inputPathHdfs = new Path(inputPath);
 
       if (!fs.exists(inputPathHdfs)) {
-        throw new RuntimeException("Input file not available: " + inputPath);
+        throw new PipelinesException("Input file not available: " + inputPath);
       }
 
       String tmpInputDir = new File(options.getTempLocation()).getParent();
@@ -77,19 +105,13 @@ public class ALADwcaToVerbatimPipeline {
     }
 
     String targetPath =
-        String.join(
-            "/",
-            options.getTargetPath(),
-            options.getDatasetId(),
-            options.getAttempt().toString(),
-            "verbatim");
-
+        PathBuilder.buildDatasetAttemptPath(
+            options, PipelinesVariables.Pipeline.Conversion.FILE_NAME, false);
     String tmpPath = PathBuilder.getTempDir(options);
 
     log.info("Input path: {}", inputPath);
     boolean isDir = Paths.get(inputPath).toFile().isDirectory();
 
-    log.info("Input path is isDir {}", isDir);
     DwcaIO.Read reader =
         isDir
             ? DwcaIO.Read.fromLocation(inputPath)
